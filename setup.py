1e69f70da17ff4f61fd2d72012cdd04252b8b123
Yang Xu <innerfirexy@gmail.com>
2017-07-21 10:19:37 -0400

    update setup meta info

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from distutils.core import setup
 
-setup(name='LIWCutils', version='1.0', py_modules=['LIWCutils'])
+setup(name='LIWCutils',
+    version='1.0',
+    description='Linguistic Inquiry and Word Count (LIWC) Utilities',
+    author='Yang Xu',
+    author_email='innerfirexy@gmail.com',
+    url='https://github.com/innerfirexy/LIWCutils',
+    py_modules=['LIWCutils'])
